0ae1ce209e6294e0a4df0a7db2fd73eec4668727
Aritra1704 <aritra1704@gmail.com>
2020-09-23 17:03:15 +0530

    Setting up first screen

--- SampleFilter/app/src/main/java/com/arpaul/samplefilter/ListFragment.kt
+++ SampleFilter/app/src/main/java/com/arpaul/samplefilter/ListFragment.kt
@@ -11,7 +11,7 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ListFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +25,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.filter_dashboard_frag)
         }
     }
 }
